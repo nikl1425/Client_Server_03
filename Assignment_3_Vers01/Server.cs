dfdd0ae32075f93a704cf50193670f368424f203
nikl1425 <niklas@astne.dk>
2020-10-19 21:12:49 +0200

    Path virker for nhr

--- Assignment_3_Vers01/Server.cs
+++ Assignment_3_Vers01/Server.cs
@@ -20,41 +20,21 @@ namespace Server
 
             while (true)
             {
-                /*var client = server.AcceptTcpClient();
-                Console.WriteLine("Accepted client!");
-
-                var stream = client.GetStream();
-                var msg = Read(client, stream);
-
-                Console.WriteLine($"Message from Server-Client {msg}");
-
-                var data = Encoding.UTF8.GetBytes(msg.ToUpper());
-                stream.Write(data);
-                
-                /*private static string Read(TcpClient client, NetworkStream stream)
-                {
-                    byte[] data = new byte[client.ReceiveBufferSize];
-                    var cnt = stream.Read(data);
-                    var msg = Encoding.UTF8.GetString(data, 0, cnt);
-                    return msg;
-                }*/
-                
-                ////////jacob/////////
+               
                 var client = server.AcceptTcpClient();
                 Console.WriteLine("Accepted client!");
 
                 var stream = client.GetStream();
                 byte[] data = new byte[client.ReceiveBufferSize];
                 var cnt = stream.Read(data);
-                var msg = Encoding.UTF8.GetString(data, 0, cnt);
                 
+                var msg = Encoding.UTF8.GetString(data, 0, cnt);
                 Console.WriteLine($"Message from client:  {msg}");
 
                 data = Encoding.UTF8.GetBytes(msg.ToUpper());
                 
                 stream.Write(data);
-                //////jacob////////
-                
+
                 var path  = @"C:\Users\45535\Desktop\RAWDATA\C#\Projects\Assignment_3_Vers01\ClassLibrary1\JsonFiles\JsonServerTest.json";
                 
                 // Write input from client to Json response file.
